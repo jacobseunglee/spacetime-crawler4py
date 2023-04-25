94f8a3eefda36e77605252a2606cae7c6b3400e2
Jacob Lee <jacoblee0001@gmail.com>
2023-04-24 17:10:45 -0700

    added checksum to catch exact duplicate pages in the frontier

--- crawler/worker.py
+++ crawler/worker.py
@@ -5,8 +5,15 @@ from utils.download import download
 from utils import get_logger
 import scraper
 import time
+from bs4 import BeautifulSoup
 
-
+def checksum(resp):
+    parsed_html = BeautifulSoup(resp.raw_response.content, "lxml")
+    text = parsed_html.get_text()
+    sum = 0
+    for character in text.strip():
+        sum += ord(character)
+    return sum
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
@@ -18,6 +25,7 @@ class Worker(Thread):
         super().__init__(daemon=True)
         
     def run(self):
+        prev = None
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
@@ -27,7 +35,14 @@ class Worker(Thread):
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
+            cur = checksum(resp)
+            if prev != None:
+                if prev != cur:
+                    scraped_urls = scraper.scraper(tbd_url, resp)
+                    prev = cur
+                else:
+                    scraped_urls = []
+            prev = checksum(resp)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-from urllib import robotparser
 
 REPEATED_TRESH = 15
 
